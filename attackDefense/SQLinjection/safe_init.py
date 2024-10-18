3173884c36709dfe7e4e88d1a1b1f43fda544697
wy520 <username@users.noreply.github.com>
2024-10-18 13:30:18 +0800

    修正icon，适应自动填充

---
+++ attackDefense/SQLinjection/safe_init.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+import time
+from myapp.function.disk_io import DiskIO
+from myapp.config import file_path, root, secret_key
+
+print('开始读取文件构建目录树，请等待')
+diskio = DiskIO(file_path, root)
+print('成功构建目录树')
+# 记录服务器启动时间戳
+SERVER_START_TIMESTAMP = str(int(time.time()))
+# 记录已经轮播的目录
+blacklisted_directories = set()
+
+
+db = SQLAlchemy()
+def create_app():
+    # 初始化 Flask 应用
+    app = Flask(__name__)
+    app.secret_key = secret_key
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 60 * 60 * 24 * 7  # 缓存一周
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # 初始化数据库
+    db.init_app(app)
+    return app
+
+# 用户模型
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+    @classmethod
+    def get(cls, username):
+        print(f'正在查询用户{username}')
+        # 手工查询示例，使用参数化查询以防SQL注入
+        query = text("SELECT * FROM user WHERE username = :username")
+        result = db.session.execute(query, {'username': username}).fetchone()
+        if result:
+            result_dict = dict(result._mapping)
+            print(f'查到匹配的数据库字段：{result_dict}')
+            # 创建包含 UserMixin 功能的 User 类实例
+            return cls(**dict(result_dict))
+        return None

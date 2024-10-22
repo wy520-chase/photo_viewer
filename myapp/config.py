8c2089a0461ffd473167b48de425e478f5c29b52
wy520 <username@users.noreply.github.com>
2024-10-22 14:42:04 +0800

    .

--- myapp/config.py
+++ myapp/config.py
@@ -3,12 +3,15 @@ import os
 # 存放目录树的文件夹路径
 file_path = 'myapp/file_tree.json'
 
-# 获取环境变量的值
+'''# 获取环境变量的值
 # 服务器密钥
 secret_key = os.getenv('SECRET_KEY')
 # 初始账户密码
 init_username = os.getenv('USER_NAME')
-init_password = os.getenv('PASSWORD')
+init_password = os.getenv('PASSWORD')'''
 # 图片根路径
 root = 'myapp/static/images'
 
+secret_key = '12345678'
+init_username = 'admin'
+init_password = 'admin'

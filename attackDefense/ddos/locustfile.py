ce2f900d8723c3c9ff775a4d38436fc83dd9d795
wy520-chase <69709332+wy520-chase@users.noreply.github.com>
2024-10-22 13:49:43 +0800

    Delete attackDefense directory

--- attackDefense/ddos/locustfile.py
+++
@@ -1,8 +0,0 @@
-from locust import HttpUser, task, between
-
-class WebsiteUser(HttpUser):
-    wait_time = between(0.1, 0.2)
-
-    @task
-    def index(self):
-        self.client.get("/", headers={"Connection": "keep-alive"})
